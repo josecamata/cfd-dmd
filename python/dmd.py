f978e13f07a474c01280de74f3afac3f3b79bc0c
Breno <breno.lino@ice.ufjf.br>
2023-03-19 20:34:47 -0300

    Adicionando código DMD

--- python/dmd.py
+++ python/dmd.py
@@ -1,3 +1,36 @@
 import numpy as np
 
-print('Hello World!!')
+def DMD(data, r):
+    """Dynamic Mode Decomposition (DMD) algorithm."""
+    
+    ## Build data matrices
+    X1 = data[:, : -1]
+    X2 = data[:, 1 :]
+
+    ## Perform singular value decomposition on X1
+    u, s, v = np.linalg.svd(X1, full_matrices = False)
+    ## Compute the Koopman matrix
+    A_tilde = u[:, : r].conj().T @ X2 @ v[: r, :].conj().T * np.reciprocal(s[: r])
+    ## Perform eigenvalue decomposition on A_tilde
+    Phi, Q = np.linalg.eig(A_tilde)
+    ## Compute the coefficient matrix
+    Psi = X2 @ v[: r, :].conj().T @ np.diag(np.reciprocal(s[: r])) @ Q
+    A = Psi @ np.diag(Phi) @ np.linalg.pinv(Psi)
+    
+    return A_tilde, Phi, A
+
+def DMD4cast(data, r, pred_step):
+    N, T = data.shape
+    _, _, A = DMD(data, r)  # É retornado a matriz de Koopman, autovalores e matriz de coeficientes
+    mat = np.append(data, np.zeros((N, pred_step)), axis = 1)
+    for s in range(pred_step):
+        mat[:, T + s] = (A @ mat[:, T + s - 1]).real
+    return mat[:, - pred_step :]
+
+X = np.zeros((2, 10))   # Gera duas listas com 10 valores cada
+X[0, :] = np.arange(1, 11)
+X[1, :] = np.arange(2, 12)
+pred_step = 2
+r = 2
+mat_hat = DMD4cast(X, r, pred_step)
+print(mat_hat)
