c3b95867e81634db6c2089aaa37f29ddc67872fb
Breno <breno.lino@ice.ufjf.br>
2023-04-24 19:12:32 -0300

    adicionando código teste vetor img

---
+++ python/import_image.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import scipy.io as scipy
+
+img = Image.open('imagem.png').convert('RGBA')
+# print(img)
+arr = np.array(img)
+
+# record the original shape
+shape = arr.shape
+
+# make a 1-dimensional view of arr
+flat_arr = arr.ravel()
+
+# convert it to a matrix
+vector = np.matrix(flat_arr)
+
+# do something to the vector
+vector[:,::10] = 128
+
+# reform a numpy array of the original shape
+arr2 = np.asarray(vector).reshape(shape)
+
+# make a PIL image
+img2 = Image.fromarray(arr2, 'RGBA')
+img2.show()
+
