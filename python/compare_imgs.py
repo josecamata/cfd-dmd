9f64c38aa4578df43a9d250e3d8110b7b70c5c7f
Breno <breno.lino@ice.ufjf.br>
2023-07-18 19:47:54 -0300

    fixing libraries

--- python/compare_imgs.py
+++ python/compare_imgs.py
@@ -1,36 +1,30 @@
-import cv2
+from PIL import Image
 import numpy as np
 import math
 
 def calculate_psnr(original_image, generated_image):
-    # Carrega as imagens como matrizes
-    img_original = cv2.imread(original_image, cv2.IMREAD_UNCHANGED)
-    img_generated = cv2.imread(generated_image, cv2.IMREAD_UNCHANGED)
+    img_original = Image.open(original_image)
+    img_generated = Image.open(generated_image)
 
-    # Verifica se as imagens têm as mesmas dimensões
-    if img_original.shape != img_generated.shape:
+    if img_original.size != img_generated.size:
         raise ValueError("As imagens têm dimensões diferentes.")
 
-    # Calcula a diferença ao quadrado entre as imagens
-    diff = np.subtract(img_original.astype(float), img_generated.astype(float))
+    arr_original = np.array(img_original, dtype=float)
+    arr_generated = np.array(img_generated, dtype=float)
+
+    diff = arr_original - arr_generated
     squared_diff = np.square(diff)
 
-    # Calcula o MSE
     mse = np.mean(squared_diff)
-
-    # Calcula o valor máximo possível para um pixel (255 para escala de cinza de 8 bits)
     max_pixel = 255
 
-    # Calcula o PSNR
     if mse == 0:
         psnr = math.inf
-
     else:
         psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
 
     return psnr
 
-# Caminho para as 100 imagens originais e geradas
 original_images = []
 generated_images = []
 
@@ -38,7 +32,6 @@ for i in range(0, 100):
     original_images += [f"/home/breno/cfd-dmd/pngs_bw/RenderView1_{i}.png"]
     generated_images += [f"/home/breno/cfd-dmd/predicted_imgs/imagem_predita_{i}.png"]
 
-# Loop para calcular o PSNR para cada par de imagens
 for i in range(len(original_images)):
     original_image = original_images[i]
     generated_image = generated_images[i]
