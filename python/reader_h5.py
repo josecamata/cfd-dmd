7fe44370eb65d83763e848908b53f64d4268a693
Breno <breno.lino@ice.ufjf.br>
2023-10-10 11:48:01 -0300

    adicionando dmd reader

--- python/reader_h5.py
+++ python/reader_h5.py
@@ -1,5 +1,7 @@
 import h5py
 import numpy as np
+from pydmd import DMD
+import dmd
 
 path_h5 = '/home/breno/cfd-dmd/python/cylinder.h5'
 file_h5 = h5py.File(path_h5, 'r')
@@ -11,5 +13,31 @@ for key in file_h5['/Function/u']:
     column = dataset[:, 0]
     extracted_column.append(column)
 
-size = np.shape(extracted_column)
-print("Dimensões do vetor criado:", size)
+data_matrix = np.array(extracted_column).T  # Transpõe para obter as snapshots como colunas
+
+r = 2
+A_tilde, Phi, A, Psi = dmd.DMD(data_matrix, r)
+t_conhecido = 2.0
+
+delta_t = 0.0025
+pred_step = int(t_conhecido / delta_t)
+
+predictions = dmd.DMD4cast(data_matrix, r, pred_step)
+print("Dados previstos no instante de tempo t =", t_conhecido)
+print(predictions)
+
+# X = np.array(extracted_column).T
+# # print(X)
+
+# dmd = DMD(svd_rank=2)
+# dmd.fit(X)
+
+# N, T = X.shape
+
+# num_predictions = 2
+# Xn = X[:, 0]
+
+# for t in range(T + num_predictions):
+#     Xn = dmd.predict(Xn)
+
+# print(Xn.T.real)
