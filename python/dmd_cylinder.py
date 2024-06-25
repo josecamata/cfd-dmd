0c47db9aed5c557fb82687416c865f57cf4b7c3e
Breno <breno.lino@ice.ufjf.br>
2024-06-25 16:07:22 -0300

    add dmd cylinder

--- python/dmd_cylinder.py
+++ python/dmd_cylinder.py
@@ -3,43 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dmd_class import DMD
 
-path_h5 = '/home/breno/cfd-dmd/python/cylinder.h5'
-file_h5 = h5py.File(path_h5, 'r')
+path_h5_predicted = '/home/breno/cfd-dmd/cylinder_h5/cylinder.h5'
+file_h5_predicted = h5py.File(path_h5_predicted, 'r+')
 
 extracted_column = []
 total_time_points = 0
 
-for key in file_h5['/Function/u']:
-    dataset = file_h5['/Function/u/' + key]
+for key in file_h5_predicted['/Function/u']:
+    dataset = file_h5_predicted['/Function/u/' + key]
     column = dataset[:, 0]
     extracted_column.append(column)
     total_time_points += 1
 
-    if total_time_points == 50:
-        break
+    # if total_time_points == 1000:
+    #     break
 
 X = np.array(extracted_column).T
-print(X.shape)
+shape = dataset.shape
+# print(X.shape)
 
 dmd = DMD()
-dmd.fit(X)
+dmd.fit(X, dt=0.0025, thresh=1.0e-3)
 
-# total_time_points = X.shape[1]
 time_interval = 0.0025
 t_values = np.arange(0, total_time_points * time_interval, time_interval)
 predicted_data = dmd.predict(t_values)
 
-X_data = np.array(predicted_data)
-print(X_data.shape)
+# Sobrescrever os dados preditos no arquivo HDF5
+for i, key in enumerate(file_h5_predicted['/Function/u']):
+    dataset = file_h5_predicted['/Function/u/' + key]
+    dataset[:, 0] = predicted_data[:, i]
 
+file_h5_predicted.close()
+
+# Calcular os erros de Frobenius entre os dados originais e previstos
 frobenius_errors = []
-
-for i in range(X_data.shape[1]):
+for i in range(predicted_data.shape[1]):
     original_data = X[:, i]
     predicted_data_at_time = predicted_data[:, i]
-    error = np.linalg.norm(original_data - predicted_data_at_time)
+    error = np.linalg.norm(original_data - predicted_data_at_time) / np.linalg.norm(original_data)
     frobenius_errors.append(error)
 
+# Plotar os erros de Frobenius
 plt.plot(np.arange(0, total_time_points * time_interval, time_interval), frobenius_errors, label='Erro de Frobenius')
 plt.xlabel('Tempo')
 plt.ylabel('Erro de Frobenius')
